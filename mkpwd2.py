aa837974d6dc35ce602da02358b7775ac550b785
Jared Dunbar <jrddunbr@gmail.com>
2018-07-30 09:19:38 -0400

    Adding mkpwd2.py, which adds the capability to have special characters.

---
+++ mkpwd2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# mkpwd.py - make a simple random password
+
+import string, random, sys
+
+def generateSecureKey(size):
+    # print("creating secure number of size " + str(size))
+    return ''.join(random.SystemRandom().choice(string.digits + string.ascii_uppercase + string.punctuation) for _ in range(size))
+
+# If an argument is passed, try to parse it as a number of characters, instead of asking for one
+if len(sys.argv) == 2:
+    try:
+        size = int(sys.argv[1])
+        if size > 0:
+            print(generateSecureKey(int(size)))
+        else:
+            print("Please enter a number greater than 0", file=sys.stderr)
+            sys.exit(1)
+    except SystemExit as e:
+        sys.exit(e)
+    except:
+        print("{} is not a number".format(sys.argv[1]), file=sys.stderr)
+else:
+    size = input("Enter size for random security key: ")
+    if size > 0:
+        print(generateSecureKey(int(size)))
+    else:
+        print("Please enter a number greater than 0")
